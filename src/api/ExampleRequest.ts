c38da9e74420688a772bbf434359d18e9c9e8290
Yu Shengyuan <1749238278@qq.com>
2019-11-23 10:10:40 +0800

    无修改

--- src/api/ExampleRequest.ts
+++ src/api/ExampleRequest.ts
@@ -1,9 +1,13 @@
 import {Request} from "@/api/config";
 
 class ExampleRequest extends Request {
-    字段增删查改(data: {
-        a: string;
-    }) {
+// {
+//     "form":{
+//         "id":"uuid" // 参看表单中获取 demo可用 ae8a7da6-d7ed-4aac-bc45-1e6dd528fa95
+//     }
+// }
+    getFormdata(data: any) {
+        //data是你发送请求时post请求的字段的字段，post方法返回一个请求函数发送请求
         return this.post('12321', data);
     }
 
--- src/store/modules/FiledManage.ts
+++ src/store/modules/FiledManage.ts
@@ -3,26 +3,28 @@ import {getFormatResult} from "@/utils/format";
 import {exampleRequest} from "@/api/ExampleRequest";
 @Module({name: 'Auth', namespaced: true, stateFactory: true})
 export default class Auth extends VuexModule {
-    public count = 12;
-    get getCount() {
-        return this.count;
-    }
+    // public count = 12;
+    // get getCount() {
+    //     return this.count;
+    // }
 
     // action 'decr' commits mutation 'decrement' when done with return value as payload
-    @Action({ commit: 'decrement' })
-    public async decr() {
-        return 3;
-    }
-
-    @Mutation
-    private decrement(delta: number) {
-        console.log('delta', delta);
-        this.count -= delta;
-    }
+    // @Action({ commit: 'decrement' })
+    // public async decr() {
+    //     return 3;
+    // }
+    //
+    // @Mutation    ·
+    // private decrement(delta: number) {
+    //     console.log('delta', delta);
+    //     this.count -= delta;
+    // }
 
     @Action
-    public async abc(data: any) {
-
-        return getFormatResult(await exampleRequest.字段增删查改(data));
+    public async getDate(data: any) {
+        //本质上需要调用这个异步函数
+        //exampleRequest函数返回this.post('12321', data)，this.post方法返回
+        //exampleRequest.getFormdata(data)返回
+        return getFormatResult(await exampleRequest.getFormdata(data));
     }
 }
